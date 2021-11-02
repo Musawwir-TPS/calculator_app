b995e4277c4855725538aa57ec04ac54bee06056
musawwirmaqsood <musawwir58@gmail.com>
2021-11-02 18:06:25 +0500

    ui

---
+++ lib/widgets/number_button_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NumberButtonWidget extends StatelessWidget {
+  const NumberButtonWidget(this.title, this.onPressFun,
+      {this.titleColor, Key? key})
+      : super(key: key);
+  final String title;
+  final Function onPressFun;
+  final Color? titleColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final double size = (MediaQuery.of(context).size.width - 80) / 4;
+    return SizedBox(
+      width: size,
+      height: size,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(), primary: Colors.white),
+        child: Text(
+          title,
+          style: Theme.of(context)
+              .textTheme
+              .headline4!
+              .copyWith(color: titleColor ?? Colors.black),
+        ),
+        onPressed: () {
+          onPressFun();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/zero_button_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ZeroButtonWidget extends StatelessWidget {
+  const ZeroButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double size = (MediaQuery.of(context).size.width - 80) / 4;
+    return SizedBox(
+      width: (size * 2) + 16,
+      height: size,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(45.0),
+            ),
+            primary: Colors.white),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            ' 0',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
